00620794a09af16e807521dbd7ee65e89b2b12a9
Богданов Павел Васильевич <pavelbog@mail.ru>
2023-01-08 21:34:09 +0500

    Получение списка изображений
    Реализовал получение списка изображений

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import hugin
+import hugin.makepanorama as mp
+
+images = []
+images.append('images/1.jpg');
+images.append('images/2.jpg');
+# while True:
+#     filename = input('Enter filename or "End": ')
+#     if filename != "End":
+#         if not os.path.exists(filename):
+#             print("Error: The file does not exist.")
+#         else:
+#             images.append(filename);
+#     else:
+#         print("generating a panoramic photo")
+#         break
+
+
+
